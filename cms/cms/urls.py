3cee64dc92082a00425d57fe33dc3d13b18eddfc
b10n1k <jbonatakis@gmail.com>
2013-09-14 19:29:50 +0300

    practical django project code. First upload

---
+++ cms/cms/urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    #(r'^weblog/$','coltrane.views.entries_index'),
+    #(r'^weblog/$','django.views.generic.date_based.archive_index',entry_info_dict),
+    #(r'^weblog/(?P<year>\d{4}/$','django.views.generic.date_based.archive_year',entry_info_dict),
+    #(r'^weblog/(?P<year>\d{4}/(?P<month>\w{3})/$','django.views.generic.date_based.archive_month',entry_info_dict),
+    #(r'^weblog/(?P<year>\d{4}/(?P<month>\w{3})/(?P<day>\d{2})/$','django.views.generic.date_based.archive_day',entry_info_dict),
+    #(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>\[-\w]+)/$','django.views.generic.date_based.object_detail',entry_info_dict),
+    #(r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$','coltrane.views.entry_details'),
+    # Examples:
+    # url(r'^$', 'cms.views.home', name='home'),
+    # url(r'^cms/', include('cms.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    #(r'^weblog/',include('coltrane.urls')),
+    (r'^weblog/categories/', include('coltrane.urls.categories')),
+    (r'^weblog/links/', include('coltrane.urls.links')),
+    (r'^weblog/tags/', include('coltrane.urls.tags')),
+    (r'^weblog/', include('coltrane.urls.entries')),
+
+    url(r'^admin/', include(admin.site.urls)),
+   
+    (r'^tinymce/(?P<path>.*)$','django.views.static.serve',{'document_root':'/mnt/home/j0ni/eeeLab/djangoLab/cms/tinymce/js/tinymce'}),
+    (r'^search/$','search.views.search'),
+    (r'',include('django.contrib.flatpages.urls')),
+)
